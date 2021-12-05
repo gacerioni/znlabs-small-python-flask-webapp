046ea2251b013fe5d518580fa6f802b43fa14c50
gacerioni <gacerioni@gmail.com>
2021-12-04 22:10:57 -0300

    First Working version!

---
+++ app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    celsius = request.args.get("celsius", "")
+    if celsius:
+        fahrenheit = fahrenheit_from(celsius)
+    else:
+        fahrenheit = ""
+    return (render_template('index.html') + fahrenheit)
+
+
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
